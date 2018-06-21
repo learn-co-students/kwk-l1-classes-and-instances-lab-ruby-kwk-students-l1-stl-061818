ba9ed01898f31a56c4111d152279323fa4f771d6
tiffanyyoon <yyoon032@rsdmo.org>
2018-06-21 21:27:12 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,13 @@
 # dog.rb
+class Dog 
+  attr_accessor :dog_name
+  def initialize(dog_name)
+    @dog_name = dog_name
+  end 
+end
+
+dog1 = Dog.new("Fido")
+dog2 = Dog.new("Snoopy")
+dog3 = Dog.new("Lassie")
+
+puts dog2.dog_name
